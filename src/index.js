f18d9e40ef3fb0c7f39a3ecb6032bcccf827e60c
rukurx <rukurx@users.noreply.github.com>
2017-01-12 16:42:36 +0900

    add redux

--- src/index.js
+++ src/index.js
@@ -1,9 +1,18 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import './index.css';
+import { render } from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import reducer from './reducers';
+import thunk from 'redux-thunk';
 
-ReactDOM.render(
-  <App />,
+import App from './App';
+
+let initialState = {};
+const store = createStore(reducer, initialState, applyMiddleware(thunk));
+
+render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
   document.getElementById('root')
 );
