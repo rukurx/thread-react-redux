de8e5b78ef8ee74ad7e8b584faf7c6eb6f44096c
rukurx <rukurx@users.noreply.github.com>
2017-01-12 17:33:53 +0900

    add layout

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,7 +2,57 @@ import React from 'react';
 
 const App = () => (
     <div>
-        default
+
+        {/* 基になる投稿 */}
+        <div className="thread">
+            <div className="user">
+                <div className="user_image"></div>
+                <div className="user_name"></div>
+            </div>
+            <div className="title"></div>
+            <div className="body"></div>
+
+            <div className="thread_editArea">
+                <input type="text"/>
+                <input type="text"/>
+                <button className="edit_save">更新</button>
+            </div>
+
+            <button className="edit">編集</button>
+            <button className="delete">削除</button>
+        </div>
+
+        {/* コメント一覧 */}
+        <div className="comments">
+            <div className="comment">
+                <div className="user">
+                    <div className="user_image"></div>
+                    <div className="user_name"></div>
+                </div>
+                <div className="title"></div>
+                <div className="body"></div>
+
+                <div className="comment_editArea">
+                    <input type="text"/>
+                    <input type="text"/>
+                    <button className="edit_save">更新</button>
+                </div>
+
+                <button className="edit">編集</button>
+                <button className="delete">削除</button>
+            </div>
+        </div>
+
+        {/* いいね！一覧 */}
+        <div className="likes">
+            <div className="like">
+                <div className="user">
+                    <div className="user_image"></div>
+                    <div className="user_name"></div>
+                </div>
+            </div>
+        </div>
+
     </div>
 );
 
