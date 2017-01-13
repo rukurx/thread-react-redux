45479e4c483c0ef1ee303cb503b52bb3ce094c63
rukurx <rukurx@users.noreply.github.com>
2017-01-13 14:32:45 +0900

    add action creators

---
+++ src/actions/index.js
@@ -0,0 +1,25 @@
+/****************************************
+■ スレッド操作
+****************************************/
+// スレッド編集ボタンクリック(編集モードにする)
+export const editThread = () => {
+    return {
+        type: 'EDIT_THREAD'
+    };
+};
+
+// スレッド内容更新
+export const updateThread = (title, body) => {
+    return {
+        type: 'UPDATE_THREAD',
+        title,
+        body,
+    };
+};
+
+// スレッド削除
+export const deleteThread = () => {
+    return {
+        type: 'DELETE_THREAD'
+    };
+};
